1c20e5fe7f369ece7263fbab6a386743ccdf86e3
Ashar Khan <asharullah.khan2468@gmail.com>
2024-05-21 15:13:28 +0500

    Bulb Project

---
+++ app.js
@@ -0,0 +1,12 @@
+let image = document.querySelector("#image")
+image.addEventListener('mouseover' , function(){
+    image.src =
+      "https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-1200-80.jpg";
+})
+
+ image.addEventListener('mouseout', function(){
+  image.src =
+    "https://t3.ftcdn.net/jpg/00/49/16/54/360_F_49165494_FgI5L9unkhUHaCDcPtUqYtRiUlx8ITjO.jpg";
+});
+
+
